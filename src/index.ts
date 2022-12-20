bffa0519ef0fd26160c0411ff7c5b6e3fa16e349
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-20 17:45:13 -0300

    Integração do front-end web com a API + implementação dos WebSockets

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './router';
+
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose.set('strictQuery', true);
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
 
+    app.use((_req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
     app.use(
       '/uploads',
       express.static(path.resolve(__dirname, '..', 'uploads')),
@@ -19,7 +31,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
